b3b8650a309b03afd67fd5c59bb6ebea22cd0390
aluno <aluno@aluno-desktop>
2013-04-09 09:59:39 -0300

    Adicionada Classe telefone

---
+++ Trabalholtp3/src/br/edu/ifnmg/tads/ltp3/trabalho1/Telefone.java
@@ -0,0 +1,12 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.tads.ltp3.trabalho1;
+/**
+ *
+ * @author Maike Jordan
+ */
+public class Telefone {
+    
+}
