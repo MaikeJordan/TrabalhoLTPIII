93a104d4a2af5c14719725288081788bbf00654e
aluno <aluno@aluno-desktop>
2013-04-09 10:51:38 -0300

    Adicionada Classe telefone

--- Trabalholtp3/src/br/edu/ifnmg/tads/ltp3/trabalho1/Email.java
+++ Trabalholtp3/src/br/edu/ifnmg/tads/ltp3/trabalho1/Email.java
@@ -1,14 +1,41 @@
-package br.edu.ifnmg.tads.ltp3.trabalho1;
-
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+package br.edu.ifnmg.tads.ltp3.trabalho1;
 
 /**
  *
- * @author aluno
+ * @author Maike Jordan
  */
 public class Email {
-    
+
+    private String NomeEmail;
+    private int IdEmail;
+
+    public Email() {
+
+        NomeEmail = "";
+        IdEmail = 0;
+    }
+
+    public String getNomeEmail() {
+        return NomeEmail;
+    }
+
+    public void setNomeEmail(String NomeEmail) {
+        for (int i = 0; i < NomeEmail.length(); i++) {
+            if (NomeEmail.indexOf(i) == '@'){
+                this.NomeEmail = NomeEmail;
+            } 
+        }
+    }
+
+    public int getIdEmail() {
+        return IdEmail;
+    }
+
+    public void setIdEmail(int IdEmail) {
+        this.IdEmail = IdEmail;
+    }
 }
